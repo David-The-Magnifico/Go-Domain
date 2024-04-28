056f5fdcb463d516e376dc6c483895c1122f3609
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-04-28 14:30:01 +0100

    Commit on 28-04-2024

---
+++ iMystery33.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	row := 5
+
+	for row >= 1 {
+		column := 5
+
+		for column >= 1 {
+			if row%2 == 0 {
+				fmt.Print("X")
+			} else {
+				fmt.Print("O")
+			}
+			column--
+		}
+
+		row--
+		fmt.Println()
+	}
+}

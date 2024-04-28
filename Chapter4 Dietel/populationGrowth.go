7ba0db491784b9566038518f636d5a1d8f9751df
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-04-28 21:01:28 +0100

    Commit-28-04-2024

--- Chapter4 Dietel/populationGrowth.go
+++ Chapter4 Dietel/populationGrowth.go
@@ -1,19 +1,23 @@
-import java.util.Scanner;
+package main
 
-public class PopulationGrowth {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the current world population: ");
-        double currentPopulation = scanner.nextDouble();
-        System.out.print("Enter the annual world population growth rate (in percentage): ");
-        double growthRate = scanner.nextDouble();
+import "fmt"
 
-        double population = currentPopulation;
-        int year = 0;
-        while (population < 2 * currentPopulation) {
-            population = population * (1 + growthRate / 100);
-            year++;
-        }
-        System.out.println("The world population will double in " + year + " years.");
-    }
+func main() {
+	var currentPopulation, growthRate float64
+
+	fmt.Print("Enter the current world population: ")
+	fmt.Scanln(&currentPopulation)
+
+	fmt.Print("Enter the annual world population growth rate (in percentage): ")
+	fmt.Scanln(&growthRate)
+
+	population := currentPopulation
+	year := 0
+
+	for population < 2*currentPopulation {
+		population = population * (1 + growthRate/100)
+		year++
+	}
+
+	fmt.Printf("The world population will double in %d years.\n", year)
 }

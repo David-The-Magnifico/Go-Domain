4e6b7347f9f40b56d7ad47865f6cea9e71c6af6c
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-04-29 10:01:01 +0100

    Light Task

---
+++ !plusOperator.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func add(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	sum := a ^ b
+	carry := (a & b) << 1
+	return add(sum, carry)
+}
+
+func main() {
+	num1 := 5
+	num2 := 7
+	fmt.Printf("Sum of %d and %d is: %d\n", num1, num2, add(num1, num2))
+}
